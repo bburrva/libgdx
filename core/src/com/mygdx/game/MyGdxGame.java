72712fc1ac2f3e7e3af5b3c6b13d5124ad44d76b
Ben <bburrva@gmail.com>
2017-01-07 17:15:41 -0500

    added movement and spacebar acceleration

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,32 +2,108 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	TextureRegion down, up, right, left, stand;
+	float x,y, xv, yv;
+	boolean faceRight, faceUp;
+
+
+	static final int WIDTH = 16;
+	static final int HEIGHT = 16;
+
+	static final int DRAW_WIDTH = WIDTH*3;
+	static final int DRAW_HEIGHT = HEIGHT*3;
+
+	static final float CAMERA_WIDTH = 10f;
+	static final float CAMERA_HEIGHT = 7f;
+
+	static float MAX_VELOCITY = 100;
+	//static final float MAX_JUMP_VELOCITY = 2000;
+	//static final int GRAVITY = -50;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Texture tiles = new Texture("tiles.png");
+		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+		down = grid[6][0];
+		up = grid[6][1];
+		stand = grid[6][2];
+		right = grid[6][3];
+		left = new TextureRegion(right);
+		left.flip(true, false);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		move();
+
+		TextureRegion img;
+		if (yv > 0) {
+			img = up;
+		} else if(yv < 0) {
+			img = down;
+		} else if (yv == 0 && xv > 0) {
+			img = right;
+		} else if (yv == 0 && xv < 0) {
+			img = left;
+		} else {
+			img = stand;
+		}
+		Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	float decelerate(float velocity) {
+		float deceleration = 0.95f; // the closer to 1, the slower the deceleration
+		velocity *= deceleration;
+		if (Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+
+	void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			yv = MAX_VELOCITY * -1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			xv = MAX_VELOCITY;
+			faceRight = true;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			xv = MAX_VELOCITY * -1;
+			faceRight = false;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			MAX_VELOCITY = 200;
+		} else {
+			MAX_VELOCITY = 100;
+		}
+
+		y += yv * Gdx.graphics.getDeltaTime();
+		x += xv * Gdx.graphics.getDeltaTime();
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
 	}
 }
